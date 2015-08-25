559e00fb35f2d474b3d4b15babcc8aae1d9e68e3
Yoong Kang Lim <yoongkang.lim@gmail.com>
2015-08-25 10:45:29 +1000

    Description etc

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'pipview',
+    'description': 'Open up your Python libraries in your editor.',
     'author': 'Yoong Kang Lim',
     'url': 'http://github.com/yoongkang/pip-view',
     'download_url': 'http://github.com/yoongkang/pip-view',
     'author_email': 'yoongkang.lim@gmail.com',
-    'version': '0.1.2',
+    'version': '0.1.3',
     'install_requires': ['pip'],
     'packages': ['pipview'],
     'entry_points': {'console_scripts': ['pip-view=pipview.view:main']},
