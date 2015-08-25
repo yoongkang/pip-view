8c9e32b0ff4a8aa7c7bfc7252450931438eeb0e6
Yoong Kang Lim <yoongkang.lim@gmail.com>
2015-08-25 14:43:49 +1000

    Force pip to be 7.1

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ config = {
     'url': 'http://github.com/yoongkang/pip-view',
     'download_url': 'http://github.com/yoongkang/pip-view',
     'author_email': 'yoongkang.lim@gmail.com',
-    'version': '0.1.4',
+    'version': '0.1.5',
     'license': 'MIT',
-    'install_requires': ['pip'],
+    'install_requires': ['pip>=7.1'],
     'packages': ['pipview'],
     'entry_points': {'console_scripts': ['pip-view=pipview.view:main']},
     'name': 'pipview'
